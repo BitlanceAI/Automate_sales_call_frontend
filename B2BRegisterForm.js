e16675821eb651984da7295e4f9e60a8b63cbef0
BitlanceAI <bitlanceai@gmail.com>
2026-01-25 15:39:16 +0530

    Update B2BRegisterForm.js

--- B2BRegisterForm.js
+++ B2BRegisterForm.js
@@ -70,7 +70,7 @@ export default function B2BRegistrationForm() {
       });
 
       try {
-        await fetch("https://bitlanceai.app.n8n.cloud/webhook/b2bregister", {
+        await fetch("https://bitlancetechhub.app.n8n.cloud/webhook/b2bregister", {
           method: "POST",
           headers: { "Content-Type": "application/json" },
           body: JSON.stringify({
